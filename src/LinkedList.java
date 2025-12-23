e663664e843643dabd912be095c6814dd069d6b3
anmolkr7 <anmolojha952@gmail.com>
2025-12-23 13:17:19 +0530

    UC2: Add 30 and 56 to linked list starting from 70 using head insertion

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -5,8 +5,13 @@ public class LinkedList {
     // Add node at the beginning
     public void add(int data) {
         Node newNode=new Node(data);
-        newNode.next=head;
-        head=newNode;
+
+        if(head==null){
+            head=newNode;
+        }else{
+            newNode.next=head;
+            head=newNode;
+        }
     }
 
     public void print() {
