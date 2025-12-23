fc06715c220dc36c7ec2fc3251340c0c5f3e4b95
anmolkr7 <anmolojha952@gmail.com>
2025-12-23 14:04:16 +0530

    UC8: Insert node with value 40 after node 30 in linked list

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,9 @@ public class Main {
         list.append(30);
         list.append(70);
 
-        // Search for 30
-        if(list.search(30)){
-            System.out.println("Node with value 30 found");
-        }else{
-            System.out.println("Node with value 30 not found");
-        }
+        // Insert 40 after 30
+        list.insertAfter(30,40);
+
+        list.print();
     }
 }
