03a0034a6c27c71603391ff39256a5e69c84efeb
anmolkr7 <anmolojha952@gmail.com>
2025-12-23 14:09:08 +0530

    UC9:size tracking and tail initialization in LinkedList operations

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,18 +2,21 @@ public class LinkedList {
 
     Node head;
     Node tail;
+    int size;
 
     // Add node at the beginning
     public void add(int data) {
         Node newNode=new Node(data);
 
         if(head==null){
-            head=newNode;
+            head=tail=newNode;
         }else{
             newNode.next=head;
             head=newNode;
         }
+        size++;
     }
+
 
     // Add at end
     public void append(int data){
@@ -24,7 +27,9 @@ public class LinkedList {
             tail.next=newNode;
             tail=newNode;
         }
+        size++;
     }
+
 
     //Insert new node after given node
     public void insertAfter(int key,int data){
@@ -38,11 +43,14 @@ public class LinkedList {
 
                 if(current==tail)
                     tail=newNode;
+
+                size++;
                 return;
             }
             current=current.next;
         }
     }
+
 
     // Delete first element
     public void pop(){
@@ -83,6 +91,36 @@ public class LinkedList {
             current=current.next;
         }
         return false;
+    }
+
+    //Delete node by key
+    public void delete(int key){
+        if(head==null)
+            return;
+
+        if(head.data==key){
+            head=head.next;
+            size--;
+            if(head==null)
+                tail=null;
+            return;
+        }
+
+        Node current=head;
+        while(current.next!=null){
+            if(current.next.data==key){
+                if(current.next==tail)
+                    tail=current;
+                current.next=current.next.next;
+                size--;
+                return;
+            }
+            current=current.next;
+        }
+    }
+
+    public int size(){
+        return size;
     }
 
     public void print() {
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@ public class Main {
     public static void main(String[] args) {
         LinkedList list=new LinkedList();
 
-        // Create list: 56->30->70
+        // Create list: 56->30->40->70
         list.append(56);
         list.append(30);
+        list.append(40);
         list.append(70);
 
-        // Insert 40 after 30
-        list.insertAfter(30,40);
+        // Delete 40
+        list.delete(40);
 
         list.print();
+        System.out.println("Size: "+list.size());
     }
 }
